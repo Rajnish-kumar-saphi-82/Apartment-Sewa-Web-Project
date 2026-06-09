51508e9b1616ec262749019df4f4151edbb3dbf5
Rajnish Kumar Saphi <rajnishrajak82@gmail.com>
2026-06-09 17:46:13 +0545

    backend repositories

--- Backend/src/repositories/auth.repository.ts
+++ Backend/src/repositories/auth.repository.ts
@@ -1,57 +1,6 @@
-// import { usersDataset } from "../models/auth.model";
-// import type { User } from "../types/auth.type.js";
-
-// /**
-//  * User Repository Interface
-//  * Defines all data access methods
-//  */
-// export interface IUserRepository {
-//   findByEmail(email: string): User | undefined;
-//   findById(id: string): User | undefined;
-//   create(user: User): User;
-//   getAll(): User[];
-// }
-
-// /**
-//  * User Repository Implementation
-//  * Handles all database operations for User entity
-//  */
-// export class UserRepository implements IUserRepository {
-  
-//   /**
-//    * Find user by email
-//    */
-//   findByEmail(email: string): User | undefined {
-//     return usersDataset.find(u => u.email === email);
-//   }
-
-//   /**
-//    * Find user by ID
-//    */
-//   findById(id: string): User | undefined {
-//     return usersDataset.find(u => u.id === id);
-//   }
-
-//   /**
-//    * Create new user
-//    */
-//   create(user: User): User {
-//     usersDataset.push(user);
-//     return user;
-//   }
-
-//   /**
-//    * Get all users
-//    */
-//   getAll(): User[] {
-//     return usersDataset;
-//   }
-// }
-
 import { UserModel, IUser } from "../models/auth.model";
 
 export class UserRepository {
-
   async findByEmail(email: string): Promise<IUser | null> {
     return await UserModel.findOne({ email });
   }
